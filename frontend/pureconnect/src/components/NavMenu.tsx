1fb4cce8b7d0f23601511e3ceca4d71459cfe764
KeeYoo <poronko2045@gmail.com>
2023-01-07 18:30:48 +0200

    Frontend: navigation menu is done

--- frontend/pureconnect/src/components/NavMenu.tsx
+++ frontend/pureconnect/src/components/NavMenu.tsx
@@ -15,42 +15,40 @@ const NavMenu = (props: {page: Page, theme: string}) => {
             </div>
             <div className={"nav-content"}>
                 <ul className={"nav-menu-items"}>
-                    <li className={"nav-menu-item"}>
+                    <li className={props.page == Page.Home ? "nav-menu-item active-page" : "nav-menu-item"}>
                         <div className={"nav-menu-item-icon-wrapper"}>
                             <FontAwesomeIcon className={'nav-icon'} icon={solid('home')} />
                         </div>
                         <div className={"nav-menu-item-text"}>Main Page</div>
                     </li>
-                    <li className={"nav-menu-item"}>
+                    <li className={props.page == Page.Search ? "nav-menu-item active-page" : "nav-menu-item"}>
                         <div className={"nav-menu-item-icon-wrapper"}>
                             <FontAwesomeIcon className={'nav-icon'} icon={solid('search')} />
                         </div>
                         <div className={"nav-menu-item-text"}>Search</div>
                     </li>
-                    <li className={"nav-menu-item"}>
+                    <li className={props.page == Page.Chats ? "nav-menu-item active-page" : "nav-menu-item"}>
                         <div className={"nav-menu-item-icon-wrapper"}>
                             <FontAwesomeIcon className={'nav-icon'} icon={solid('comments')} />
                         </div>
                         <div className={"nav-menu-item-text"}>Chats</div>
                     </li>
-                    <li className={"nav-menu-item"}>
+                    <li className={props.page == Page.Notifications ? "nav-menu-item active-page" : "nav-menu-item"}>
                         <div className={"nav-menu-item-icon-wrapper"}>
                             <FontAwesomeIcon className={'nav-icon'} icon={solid('bell')} />
                         </div>
                         <div className={"nav-menu-item-text"}>Notifications</div>
                     </li>
-                    <li className={"nav-menu-item"}>
+                    <li className={props.page == Page.Profile ? "nav-menu-item active-page" : "nav-menu-item"}>
                         <img src="" alt=""/>
                         <div className={"nav-menu-item-text"}>Profile</div>
                     </li>
                 </ul>
-
                 <div className={"nav-more"}>
                     <FontAwesomeIcon className={'nav-icon'} icon={solid('bars')} />
                     <div className={"nav-menu-item-text"}>More</div>
                 </div>
             </div>
-
         </div>
     );
 };
