2e7da2ddec65d35bb69881825973070b56c6cc7e
KeeYoo <poronko2045@gmail.com>
2023-01-16 23:27:39 +0200

    Frontend: fixed edit profile bug with changing user profile info when typing new info in modal window - user objects are separated

--- frontend/pureconnect/src/components/MyProfileInfo.tsx
+++ frontend/pureconnect/src/components/MyProfileInfo.tsx
@@ -25,12 +25,17 @@ const MyProfileInfo = (props: {
     setUser?: React.Dispatch<SetStateAction<IUser|undefined>>
 }) => {
 
+    const [user, setUser] = useState<IUser|undefined>(props.user);
+
     useEffect(() => {
         const getUser = async () => {
             const token = localStorage.getItem('access_token')
             if (token) {
                 const response = await getMyProfile(token);
-                props.setUser!(response)
+                if (props.isEdit)
+                    setUser(response);
+                else
+                    props.setUser!(response)
             }
         }
         getUser()
@@ -65,7 +70,7 @@ const MyProfileInfo = (props: {
     const convertAvatarImageToBase64 = (reader: FileReader, file: File) => {
         reader.onload = (event) => {
             if (props.user)
-                props.setUser!({...props.user, avatar: event.target?.result as string});
+                setUser!({...user!, avatar: event.target?.result as string});
         };
         reader.readAsDataURL(file);
     }
@@ -83,16 +88,30 @@ const MyProfileInfo = (props: {
                                     onChange={(e) => handleChange(e)}
                                 />
                             }
-                            <img className={'my-profile-user-avatar'} src={isValidAvatar? props.user?.avatar : userDefault} alt=""/>
+                            {props.isEdit ?
+                            <img className={'my-profile-user-avatar'} src={isValidAvatar? user?.avatar : userDefault} alt=""/>
+                                :
+                            <img className={'my-profile-user-avatar'} src={isValidAvatar? props.user?.avatar : userDefault} alt=""/>}
                         </div>
-                        <ProfileUserNameEmailBlock setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>
+                        {props.isEdit ?
+                        <ProfileUserNameEmailBlock setUser={setUser!} isEdit={props.isEdit} user={user}/>
+                            :
+                        <ProfileUserNameEmailBlock setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>}
                     </div>
 
                     <div className={'my-profile-user-right'}>
-                        <MyProfileName setIsToggleProfile={props.setIsToggleProfile} isToggleProfile={props.isToggleProfile} setUser={props.setUser!} isEdit={props.isEdit} setIsOpenEditProfile={props.setIsOpenEditProfile} isActiveEditProfile={props.isActiveEditProfile} user={props.user}/>
-                        <ProfileStatusBlock isOpenFollowers={props.isOpenFollowers} setIsOpenFollowers={props.setIsOpenFollowers} isOpenFriends={props.isOpenFriends} setIsOpenFriends={props.setIsOpenFriends}  setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>
-                        <ProfileAdditionalBlock setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>
-                    </div>
+                        {props.isEdit ?
+                            <div>
+                                <MyProfileName setIsToggleProfile={props.setIsToggleProfile} isToggleProfile={props.isToggleProfile} setUser={setUser!} isEdit={props.isEdit} setIsOpenEditProfile={props.setIsOpenEditProfile} isActiveEditProfile={props.isActiveEditProfile} user={user}/>
+                                <ProfileStatusBlock isOpenFollowers={props.isOpenFollowers} setIsOpenFollowers={props.setIsOpenFollowers} isOpenFriends={props.isOpenFriends} setIsOpenFriends={props.setIsOpenFriends}  setUser={setUser!} isEdit={props.isEdit} user={user}/>
+                                <ProfileAdditionalBlock setUser={setUser!} isEdit={props.isEdit} user={user}/>
+                            </div> :
+                            <div>
+                                <MyProfileName setIsToggleProfile={props.setIsToggleProfile} isToggleProfile={props.isToggleProfile} setUser={props.setUser!} isEdit={props.isEdit} setIsOpenEditProfile={props.setIsOpenEditProfile} isActiveEditProfile={props.isActiveEditProfile} user={props.user}/>
+                                <ProfileStatusBlock isOpenFollowers={props.isOpenFollowers} setIsOpenFollowers={props.setIsOpenFollowers} isOpenFriends={props.isOpenFriends} setIsOpenFriends={props.setIsOpenFriends}  setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>
+                                <ProfileAdditionalBlock setUser={props.setUser!} isEdit={props.isEdit} user={props.user}/>
+                            </div>}
+                        </div>
                 </div> :
                 <Loader theme={props.theme}/>
             }
