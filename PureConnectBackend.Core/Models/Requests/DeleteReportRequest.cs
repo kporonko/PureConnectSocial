c594fa8627aa8b3ae270ae0f2e12e8fdffc01000
KeeYoo <poronko2045@gmail.com>
2023-02-07 13:08:11 +0200

    Styles of report card are fixed

---
+++ PureConnectBackend.Core/Models/Requests/DeleteReportRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureConnectBackend.Core.Models.Requests
+{
+    public class DeleteReportRequest
+    {
+        public int ReportId { get; set; }
+    }
+}
